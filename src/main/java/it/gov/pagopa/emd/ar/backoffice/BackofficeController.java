7ce96befdf20ce9706182b14753ad2965cf678f2
fabio.picano <fabio.picano@nttdata.com>
2026-04-01 16:42:59 +0200

    Update controller

--- src/main/java/it/gov/pagopa/emd/ar/backoffice/BackofficeController.java
+++ src/main/java/it/gov/pagopa/emd/ar/backoffice/BackofficeController.java
@@ -1,16 +1,16 @@
 package it.gov.pagopa.emd.ar.backoffice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/backoffice") 
-public interface BeckofficeController {
+public interface BackofficeController {
 
-    @GetMapping("/auth/api/auth/pagopa")
+    @PostMapping("api/auth/pagopa")
     public ResponseEntity<ResponseDTO> getToken(@RequestBody RequestDTO token);
 
 }

988b589f984f1eff1bb8988a5832567a1439bcc2
fabio.picano <fabio.picano@nttdata.com>
2026-04-01 17:23:15 +0200

    Update endpoint

--- src/main/java/it/gov/pagopa/emd/ar/backoffice/BackofficeController.java
+++ src/main/java/it/gov/pagopa/emd/ar/backoffice/BackofficeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/backoffice") 
+@RequestMapping("emd/backoffice")
 public interface BackofficeController {
 
     @PostMapping("api/auth/pagopa")
